4725aaf2814cdd043e7fc15d3c617a0e378110e6
Haojin Gui <haojig@amazon.com>
2021-05-30 23:01:00 -0700

    Add stage

--- src/main/java/com/guihaojin/examples/GuiceTestMain.java
+++ src/main/java/com/guihaojin/examples/GuiceTestMain.java
@@ -2,13 +2,14 @@ package com.guihaojin.examples;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Stage;
 import com.guihaojin.examples.modules.AoPModule;
 import com.guihaojin.examples.modules.FooModule;
 
 public class GuiceTestMain {
 
     public static void main(String[] args) {
-        Injector injector = Guice.createInjector(
+        Injector injector = Guice.createInjector(Stage.PRODUCTION,
             new FooModule(),
             new AoPModule()
         );
